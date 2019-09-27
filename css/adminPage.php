2752098a27117f10cedd6b05e0ff7f9723a78189
Sofiyah123 <musasophiat@gmail.com>
2019-09-27 11:38:00 -0700

    uploading first file

---
+++ css/adminPage.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Page</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap-3.3.7/bootstrap-3.3.7/dist/css/bootstrap.min.css">
+	<script type="text/javascript" src="boostrap-3.3.7/boostrap-3.3.7/dist/js/boostrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		
+	</div>
+</body>
+</html>
